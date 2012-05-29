d47a91d88c561f416cb13d6bd4bff4205b889379
Micah Martin <micahlmartin@gmail.com>
2012-05-29 11:58:38 -0400

    added helicon rewriter

---
+++ ISAPI_Rewrite/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+  Install-ChocolateyPackage 'ISAPI_Rewrite' 'MSI' '/quiet' 'http://www.helicontech.com/download/isapi_rewrite/ISAPI_Rewrite3_0068.msi' 'http://www.helicontech.com/download/isapi_rewrite/ISAPI_Rewrite3_0068_x64.msi'  -validExitCodes @(0)
