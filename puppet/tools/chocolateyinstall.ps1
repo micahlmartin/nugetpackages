52c06d98412f7b605c630ce61728720489d5aec3
Rob Reynolds <ferventcoder@gmail.com>
2013-06-05 12:18:35 -0500

    updating puppet

--- puppet/tools/chocolateyinstall.ps1
+++ puppet/tools/chocolateyinstall.ps1
@@ -1,1 +1,3 @@
-Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'https://downloads.puppetlabs.com/windows/puppet-2.7.17.msi' -validExitCodes @(0)
+#Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-2.7.21.msi' -validExitCodes @(0)
+Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-3.2.1.msi' -validExitCodes @(0)
+
