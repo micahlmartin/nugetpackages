3f16f6bfe053ca834fcc51d0cdadc9f6b36d4db3
Rob Reynolds <ferventcoder@gmail.com>
2013-08-16 13:13:27 -0500

    puppet 2.7.23 / 3.2.3 / 3.2.4

--- puppet/tools/chocolateyinstall.ps1
+++ puppet/tools/chocolateyinstall.ps1
@@ -1,3 +1,3 @@
-#Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-2.7.21.msi' -validExitCodes @(0)
-Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-3.2.1.msi' -validExitCodes @(0)
+#Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-2.7.23.msi' -validExitCodes @(0)
+Install-ChocolateyPackage 'puppet' 'MSI' '/qn' 'http://puppetlabs.com/downloads/windows/puppet-3.2.4.msi' -validExitCodes @(0)
 
